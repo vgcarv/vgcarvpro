12154297c73f9078124fbb31c14290b512fd2131
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-17 16:54:49 -0300

    loma

--- applications/EsteEuQuero/models/db.py
+++ applications/EsteEuQuero/models/db.py
@@ -153,3 +153,4 @@ if configuration.get('scheduler.enabled'):
 # after defining tables, uncomment below to enable auditing
 # -------------------------------------------------------------------------
 # auth.enable_record_versioning(db)
+import lomadee
