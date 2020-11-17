c653989855ddc4d256e1226683173fab3f6039bd
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-17 17:49:38 -0300

    Update lomadee.py

--- applications/EsteEuQuero/models/lomadee.py
+++ applications/EsteEuQuero/models/lomadee.py
@@ -2,7 +2,7 @@ def conecta():
     import lomadeepy
     app_token = '1605637282724491d653c'
     source_id = '36865079'
-    loma=lomadeepy.lomadee(app_token,source_id)
+    loma=lomadeepy.Lomadee(app_token,source_id)
     return loma
 def loma_conecta():
     import lomadeepy
