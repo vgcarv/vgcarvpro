211c5de4e8f7a83b7b7aef704cbd2a7146448a24
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-17 20:42:42 -0300

    Delete lomadee.py

--- applications/EsteEuQuero/models/lomadee.py
+++
@@ -1,7 +0,0 @@
-def conecta():
-    app_token = '1605637282724491d653c'
-    source_id = '36865079'
-    import lomadeepy
-    loma = lomadeepy.Lomadee(app_token, source_id)
-    #categoria=lomadeepy.Categories.all(loma)
-    return(loma)
