4b02483f0e28f06ebf6eff4169263d72fc24137f
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-17 18:27:11 -0300

    Update default.py

--- applications/EsteEuQuero/controllers/default.py
+++ applications/EsteEuQuero/controllers/default.py
@@ -8,7 +8,9 @@
 @auth.requires_membership('admin')
 def index():
     import lomadeepy
-    resp = dir(lomadeepy.Categories.all(conecta()))
+    app_token = '1605637282724491d653c'
+    source_id = '36865079'
+    resp = dir(lomadeepy.Categories.all(lomadeepy.Lomadee(app_token, source_id)))
     response.flash = T("Welcome")
     return dict(message=T('Este é o site de suporte a esteeuquero.com.br!'),resp=resp)
 
