682c44fa96dec44bc590bda15806e35609a92861
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-17 17:57:25 -0300

    s

--- applications/EsteEuQuero/models/lomadee.py
+++ applications/EsteEuQuero/models/lomadee.py
@@ -1,7 +1,7 @@
 def conecta():
-    import lomadeepy
     app_token = '1605637282724491d653c'
     source_id = '36865079'
-    loma=lomadeepy.Lomadee(app_token,source_id)
+    import lomadeepy
+    loma = lomadeepy.Lomadee(app_token, source_id)
     categoria=lomadeepy.Categories.all(loma)
     return(categoria)
