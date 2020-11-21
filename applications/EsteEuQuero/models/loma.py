86a140425e1956b57bf1a21ad40d7fbb25654ad1
vgcarvpro <vgcarvpro@users.noreply.github.com>
2020-11-21 11:33:44 -0300

    Update loma.py

--- applications/EsteEuQuero/models/loma.py
+++ applications/EsteEuQuero/models/loma.py
@@ -1,7 +1,9 @@
 def lomaCredencial():
+    import os
+    fp = os.path.join(request.folder,'private','lomadee.ini')    
     from configparser import ConfigParser
     config = ConfigParser()
-    config.read('application/EsteEuQuero/private/lomadee.ini')
+    config.read(fp)
     app_token = config.get('rest', 'app_token')
     source_id = config.get('rest', 'source_id') 
     return(app_token, source_id)
